a124d926e3076d2b8bcdc5618ced90666b43783c
KorinnaG <korinna.gladhill@gmail.com>
2025-02-26 15:08:53 -0500

    Script créé et ajouté

---
+++ Assets/Scripts/MouvementSousMarin.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class ControlePoissonDash : MonoBehaviour
+{
+    private Rigidbody _rb;
+
+    void Start()
+    {
+        _rb = GetComponent<Rigidbody>();
+    }
+
+    void OnAvancer()
+    {
+        
+        _rb.AddRelativeForce(Vector3.forward * 1f, ForceMode.VelocityChange);
+    }
+}
